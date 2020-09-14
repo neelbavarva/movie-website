cd15935f28c6051889461a1b631087655bc6bdff
Neel Bavarva <neelbavarva5@gmail.com>
2020-09-14 19:16:14 +0530

    pagination added

--- movies/src/components/top10.jsx
+++ movies/src/components/top10.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {getMovies} from '../services/fakeMovieService';
+import Pagination from './pagination';
+import {paginate} from '../utils/paginate';
 
 class Top10 extends Component {
 
     state = {
-        movies : getMovies()
-    }
+        movies : getMovies(),
+        pageSize: 4,
+        currentPage: 1
+    };
+
+    handlePageChange = page => {
+        this.setState({currentPage:page});
+    };
 
     render() { 
+        const {length: count} = this.state.movies;
+        const {pageSize, currentPage, movies: allMovies} = this.state;
+        const movies = paginate(allMovies,currentPage,pageSize);
         return ( 
             <div> <br/>
                 <h6 className="offset-1">Top10 movies </h6>
                 <br/>                 
                 <tbody>
-                    {this.state.movies.map(movie => 
+                    {movies.map(movie => 
                         <tr>
                             <div className="card offset-3" >
                             <div class="row no-gutters">
@@ -30,8 +41,19 @@ class Top10 extends Component {
                             </div>
                             </div><br/>
                         </tr>
+                        
                         )}
+                        <div className="offset-3">
+                        <Pagination itemsCount={count}
+                                    pageSize={pageSize}
+                                    onPageChange={this.handlePageChange}
+                                    currentPage={currentPage}
+                        />
+                        </div>
+                        
+                        
                 </tbody>
+                
                 
             </div>
          );
