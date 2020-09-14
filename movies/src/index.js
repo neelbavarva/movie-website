31ca0efca650dbe7caf155a5f0c02470a69a3c1f
Neel Bavarva <neelbavarva5@gmail.com>
2020-09-14 05:57:24 +0530

    movies added

--- movies/src/index.js
+++ movies/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './components/home';
+// import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home/>
   </React.StrictMode>,
   document.getElementById('root')
 );
