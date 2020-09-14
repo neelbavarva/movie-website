6c9aa07b560a8d81fe1d9eb12261ddf017f3b3af
Neel Bavarva <neelbavarva5@gmail.com>
2020-09-14 12:38:17 +0530

    card

--- movies/src/components/top10.jsx
+++ movies/src/components/top10.jsx
@@ -14,8 +14,14 @@ class Top10 extends Component {
                 <tbody>
                     {this.state.movies.map(movie => 
                         <tr>
-                            <td>{movie.title}</td>
-                            <td>{movie.title ==="Interstellar" ? <img width="150px" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB" /> : "no"}</td>
+                            <div className="card offset-3" >
+                                <img width="150px" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB"/>
+                                <div class="card-body">
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div><br/>
+                            {/* <td>{movie.title}</td>
+                            <td>{movie.title ==="Interstellar" ? <img width="150px" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB" /> : "no"}</td> */}
                         </tr>
                         )}
                 </tbody>
