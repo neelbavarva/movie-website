d2c7cb5b5c762da8040d8bb5e620dfbc43c47703
Neel Thakker <neelthakker2001@gmail.com>
2020-09-15 18:02:42 +0530

    Modified a bit

--- movies/src/components/top10.jsx
+++ movies/src/components/top10.jsx
@@ -1,85 +1,84 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import {getMovies} from '../services/fakeMovieService';
-import Pagination from './pagination';
-import {paginate} from '../utils/paginate';
-import ReactPlayer from "react-player"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
+import ReactPlayer from "react-player";
 
 class Top10 extends Component {
+  state = {
+    movies: getMovies(),
+    pageSize: 5,
+    currentPage: 1,
+  };
 
-    state = {
-        movies : getMovies(),
-        pageSize: 5,
-        currentPage: 1
-    };
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
 
-    handlePageChange = page => {
-        this.setState({currentPage:page});
-    };
-
-    render() { 
-        const {length: count} = this.state.movies;
-        const {pageSize, currentPage, movies: allMovies} = this.state;
-        const movies = paginate(allMovies,currentPage,pageSize);
-        return ( 
-            <div className="bg-color"> <br/>
-                <h6 className="offset-1">Top10 movies </h6>
-                <br/>   
-                
-                <tbody>
-                    {movies.map(movie => 
-                    
-                        <tr>
-                            <div className="row">
-                            <div className="col-9">
-                            <div className="card offset-3" >
-                            <div class="row no-gutters">
-                                <div className="col-sm-5">
-                                <img width="195px" src={movie.image}/>
-                                </div>
-                                <div class="col-sm-7">
-                                    <div class="card-body offset-1">
-                                        <h5 class="card-title">#{movie._id}  {movie.title}</h5>
-                                        <p class="card-text">{movie.year} ‧ {movie.genre.name} ‧ {movie.time}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            </div>
-                            </div>
-                            <div className="col-3">
-                            <ReactPlayer
-                                className="videoFrame"
-                                light={true}
-                                playing
-                                controls
-                                width="500px"
-                                height="280px"
-                                url={movie.url}
-                            /> 
-                            </div>
-                            </div>
-                                
-                            <br/><br/><br/>
-                        </tr>
-                        
-                        
-                        )}
-                        <div className="offset-3">
-                        <Pagination itemsCount={count}
-                                    pageSize={pageSize}
-                                    onPageChange={this.handlePageChange}
-                                    currentPage={currentPage}
-                        />
+  render() {
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const movies = paginate(allMovies, currentPage, pageSize);
+    return (
+      <div className="bg-color">
+        {" "}
+        <br />
+        <h6 className="offset-1">Top10 movies </h6>
+        <br />
+        <tbody>
+          {movies.map((movie) => (
+            <tr>
+              <div className="row">
+                <div className="col-9">
+                  <div className="card offset-3">
+                    <div class="row no-gutters">
+                      <div className="col-sm-5">
+                        <img width="195px" src={movie.image} />
+                      </div>
+                      <div class="col-sm-7">
+                        <div class="card-body offset-1">
+                          <h5 class="card-title">
+                            #{movie._id} {movie.title}
+                          </h5>
+                          <p class="card-text">
+                            {movie.year} ‧ {movie.genre.name} ‧ {movie.time}
+                          </p>
                         </div>
-                        
-                        
-                </tbody>
-                
-                
-            </div>
-         );
-    }
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3">
+                  <ReactPlayer
+                    className="videoFrame"
+                    light={true}
+                    playing
+                    controls
+                    width="500px"
+                    height="280px"
+                    url={movie.url}
+                  />
+                </div>
+              </div>
+
+              <br />
+              <br />
+              <br />
+            </tr>
+          ))}
+          <div className="offset-3">
+            <Pagination
+              itemsCount={count}
+              pageSize={pageSize}
+              onPageChange={this.handlePageChange}
+              currentPage={currentPage}
+            />
+          </div>
+        </tbody>
+      </div>
+    );
+  }
 }
- 
+
 export default Top10;
